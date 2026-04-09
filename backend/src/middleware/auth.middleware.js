f9b3f7ed5acbcf5735305f70d78467e0767abc44
PHOUN.PHAN <PHOUN.PHAN@pnc.local>
2026-04-09 18:36:52 +0700

    add file

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -2,7 +2,6 @@ import jwt from 'jsonwebtoken'
 
 const authMiddleware = {
     // verify JWT token
-
     verify: (req, res, next) =>{
         const token = req.headers.authorization?.split(' ')[1] // Bearer token
         if(!token){
@@ -10,6 +9,7 @@ const authMiddleware = {
                 message: 'Unauthorized'
             })
         }
+        
         try{
             const decoded = jwt.verify(token, process.env.JWT_SECRET)
             req.user = decoded
@@ -41,6 +41,5 @@ const authMiddleware = {
         next()
     }
 }
-
 
 export default authMiddleware
